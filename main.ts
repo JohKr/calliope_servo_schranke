58bf59ab2fa8da8011bf8c3339b48049b0aaba0d
JohKr <91758568+JohKr@users.noreply.github.com>
2024-02-11 17:43:58 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (S1 == 0) {
         S1 = 90
         for (let Index = 0; Index <= S1; Index++) {
-            pins.servoWritePin(AnalogPin.P1, Index)
+            pins.servoWritePin(AnalogPin.P3, Index)
             basic.pause(50)
         }
     } else {
         S1 = 90
         for (let Index = 0; Index <= S1; Index++) {
             Winkel = S1 - Index
-            pins.servoWritePin(AnalogPin.P1, Winkel)
+            pins.servoWritePin(AnalogPin.P3, Winkel)
             basic.pause(50)
         }
         S1 = 0
@@ -17,6 +17,6 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 let Winkel = 0
 let S1 = 0
-basic.showString("hi!")
+basic.showString("HI2")
 S1 = 0
-pins.servoWritePin(AnalogPin.P1, S1)
+pins.servoWritePin(AnalogPin.P3, S1)
